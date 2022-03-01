87756e7350b9d1971b9032a01e359c1869c6290e
朱林忠 <wb_zhulinzhong@meituan.com>
2022-03-01 20:10:31 +0800

    project init

---
+++ fastdfs.server/src/main/java/com.zlz.file.fastdfs/FileFastdfsServerApplication.java
@@ -0,0 +1,18 @@
+package com.zlz.file.fastdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot starter
+ *
+ * @author zhulinzhong
+ */
+@SpringBootApplication
+public class FileFastdfsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileFastdfsServerApplication.class, args);
+    }
+
+}
